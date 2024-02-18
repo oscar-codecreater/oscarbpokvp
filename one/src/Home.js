805e87fb1923ffe7668a02cc137feee277041fcb
oscar-codecreator <157099274+oscar-codecreator@users.noreply.github.com>
2024-02-18 17:59:11 +0530

    Title changed

--- one/src/Home.js
+++ one/src/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
             <div className='content'>
                 <p>Welcome</p>
                 <p>To</p>
-                <p>Oscar Private ltd</p>
+                <p>Oscar Private Ltd.,</p>
                 <p>Data Entry Services</p>
                <Link to='contact'> <button  className='button'>Contact-Us</button></Link>
             </div>
